e7577989f741541b943397b0e2c49c83e3e39409
Harkamal Khabra <khabra07@gmail.com>
2019-03-11 23:45:51 +0000

    updated secrets and required it

--- download_avatars.js
+++ download_avatars.js
@@ -1,12 +1,25 @@
 var request = require('request');
+var githubToken = require('./secrets.js');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
 function getRepoContributors(repoOwner, repoName, cb) {
+  var options = {
+    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+    headers: {
+      'User-Agent': 'request',
+      'Authorization': 'token' + githubToken.GITHUB_TOKEN
+    }
+  };
 
+  request(options, function(err, res, body) {
+    var data = JSON.parse(body);
+    console.log(data[0]);
+    cb(err, body);
+  });
 }
 
 getRepoContributors("jquery", "jquery", function (err, result) {
-  console.log("Errors:", err);
-  console.log("Result:", result);
+  // console.log("Errors:", err);
+  // console.log("Result:", result);
 });
