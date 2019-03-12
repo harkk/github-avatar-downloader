a0cb4072609cb0ce71500096f5962b6b428341a7
Harkamal Khabra <khabra07@gmail.com>
2019-03-12 00:49:40 +0000

    added loop in callback to iterate and console.log of each avatar_url

--- download_avatars.js
+++ download_avatars.js
@@ -8,18 +8,21 @@ function getRepoContributors(repoOwner, repoName, cb) {
     url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
     headers: {
       'User-Agent': 'request',
-      'Authorization': 'token' + githubToken.GITHUB_TOKEN
+      'Authorization': 'token ' + githubToken.GITHUB_TOKEN,
     }
   };
 
   request(options, function(err, res, body) {
     var data = JSON.parse(body);
-    console.log(data[0]);
-    cb(err, body);
+    // console.log(data[0]);
+    cb(err, data);
   });
 }
 
 getRepoContributors("jquery", "jquery", function (err, result) {
-  // console.log("Errors:", err);
+  for (var i = 0; i < result.length; i++) {
+    console.log('Result:', result[i].avatar_url);
+  } //loop for array
+  console.log("Errors:", err);
   // console.log("Result:", result);
 });
